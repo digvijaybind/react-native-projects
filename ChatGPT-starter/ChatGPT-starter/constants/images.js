40d2da537e5174a08a68336239d5fc414fd9573e
digvijaybind <binddigvijay123@gmail.com>
2024-02-02 12:27:47 +0530

    all react native projects

---
+++ ChatGPT-starter/ChatGPT-starter/constants/images.js
@@ -0,0 +1,6 @@
+const logo = require('../assets/images/logo.png')
+const avatar = require('../assets/images/avatar.jpg')
+export default {
+    logo,
+    avatar
+}
---
+++ ChatGPT-starter/ChatGPT-starter/constants/theme.js
@@ -0,0 +1,35 @@
+import { Dimensions } from 'react-native'
+const { height, width } = Dimensions.get('window')
+
+export const COLORS = {
+    primary: '#10A37F',
+    white: '#FFFFF',
+    secondaryWhite: '#F7F7F8',
+    black: '#0B0B0B',
+    secondaryBlack: '#444654',
+    tertiaryBlack: '#202123',
+    gray: 'F8F8F8',
+    secondaryGray: '#808080s',
+}
+
+export const SIZES = {
+    //Global SIZES
+    base: 8,
+    font: 14,
+    radius: 30,
+    padding: 8,
+    padding2: 12,
+    padding3: 16,
+
+    //FONTS Sizes
+    largeTitle:50,
+    h1:30,
+    h2:22,
+    h3:20,
+    h4:18,
+
+    
+
+}
+
+
